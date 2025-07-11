65bfbc9447998f82e65150998a0ccdef0d04d3a9
Ignatious Rozario <ignatiousr80@gmail.com>
2025-07-11 21:14:13 +0600

    Update product-list.tsx

--- components/shared/product/product-list.tsx
+++ components/shared/product/product-list.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import ProductCard from "./product-card";
 
 const ProductList = ({ data, title, limit }: { data: any; title?: string; limit?: number; }) => {
