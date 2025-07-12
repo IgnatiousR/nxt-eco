a39e4d767861cb19e4fe87c136207bbfb3cb74a9
Ignatious Rozario <ignatiousr80@gmail.com>
2025-07-12 18:03:42 +0600

    Create seed.ts

---
+++ db/seed.ts
@@ -0,0 +1,13 @@
+// import { PrismaClient } from "@prisma/client";
+import { prisma } from "./prisma";
+import sampleData from "./sample-data";
+
+async function main() {
+  await prisma.product.deleteMany();
+
+  await prisma.product.createMany({ data: sampleData.products });
+
+  console.log("Database seeded successfully.");
+}
+
+main();
