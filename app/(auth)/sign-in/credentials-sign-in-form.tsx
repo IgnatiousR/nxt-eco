d0ad0b962c635ec5559f2ca34f0369a32e120740
Ignatious Rozario <ignatiousr80@gmail.com>
2025-07-13 17:04:05 +0600

    Update credentials-sign-in-form.tsx

--- app/(auth)/sign-in/credentials-sign-in-form.tsx
+++ app/(auth)/sign-in/credentials-sign-in-form.tsx
@@ -8,12 +8,16 @@ import Link from "next/link";
 import { useActionState } from "react";
 import { useFormStatus } from "react-dom";
 import { signInWithCredentials } from "@/lib/actions/user.actions";
+import { useSearchParams } from "next/navigation";
 
 const CredentailsSignInForm = () => {
   const [data, action] = useActionState(signInWithCredentials, {
     success: false,
     message: "",
   });
+
+  const searchParams = useSearchParams();
+  const callbackUrl = searchParams.get("callbackUrl") || "/";
 
   const SignInButton = () => {
     const { pending } = useFormStatus();
@@ -27,6 +31,7 @@ const CredentailsSignInForm = () => {
 
   return (
     <form action={action}>
+      <input type="hidden" name="callbackurl" value={callbackUrl} />
       <div className="space-y-6">
         <div>
           <Label htmlFor="email" className="mb-2">
