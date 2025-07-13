cf14187c5ba37f1457cccc0c126846461a9697b7
Ignatious Rozario <ignatiousr80@gmail.com>
2025-07-13 17:04:01 +0600

    Update footer.tsx

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,14 +1,16 @@
 import { APP_NAME } from "@/lib/constants";
-import { Copyright } from 'lucide-react';
+import { Copyright } from "lucide-react";
 
 const Footer = () => {
   const currentYear = new Date().getFullYear();
 
-  return (<footer className="border-t">
-    <div className="p-5 flex-center">
-      <Copyright/> {currentYear} {APP_NAME}. All Rights Reserved
-    </div>
-  </footer>);
-}
- 
+  return (
+    <footer className="border-t">
+      <div className="p-5 flex-center text-sm">
+        <Copyright /> {currentYear} {APP_NAME}. All Rights Reserved
+      </div>
+    </footer>
+  );
+};
+
 export default Footer;
