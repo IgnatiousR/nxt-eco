27b5b56dd1d1ce59e5eb65305c5414b8f69b47fe
Ignatious Rozario <ignatiousr80@gmail.com>
2025-07-13 17:03:54 +0600

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  images: {
-    domains: ['ik.imagekit.io'], // image domain
-  },
+  // images: {
+  //   domains: ['ik.imagekit.io'], // image domain
+  // },
+  // images: {
+  //   remotePatterns: [new URL('https://ik.imagekit.io')],
+  // },
 };
 
 export default nextConfig;
