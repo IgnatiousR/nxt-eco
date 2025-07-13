04ea55a5fdb2be49f8822f9dbff4ab83d0dec98b
Ignatious Rozario <ignatiousr80@gmail.com>
2025-07-13 16:48:08 +0600

    Update user.actions.ts

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -5,17 +5,20 @@ import { signIn, signOut } from "@/auth";
 import { isRedirectError } from "next/dist/client/components/redirect-error";
 
 //Sign in the user with credentials
-export async function signWithCredentials( prevState: unknown, formData: FormData ) {
+export async function signInWithCredentials( prevState: unknown, formData: FormData ) {
     try {
+        console.log("Try");
         const user = signInFormSchema.parse({
             email: formData.get('email'),
             password: formData.get('password')
         });
+        console.log(user);
 
         await signIn('credentials', user);
 
         return {success: true, message: 'Signed in'}
     } catch (error) {
+        console.log('Erroe');
         if (isRedirectError(error)) throw error;
         return {success: false, message: 'Invalid credentials. Failed to sign in.'}
     }
