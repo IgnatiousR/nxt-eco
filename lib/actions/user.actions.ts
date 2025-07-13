4bf5f921672e94c009b88b73506d89c4fe056cfe
Ignatious Rozario <ignatiousr80@gmail.com>
2025-07-13 17:39:55 +0600

    Update user.actions.ts

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -1,24 +1,23 @@
 'use server'
 
-import { signInFormSchema } from "../validator";
+import { signInFormSchema, signUpFormSchema } from "../validator";
 import { signIn, signOut } from "@/auth";
 import { isRedirectError } from "next/dist/client/components/redirect-error";
+import { hashSync } from "bcrypt-ts-edge";
+import { prisma } from "@/db/prisma";
 
 //Sign in the user with credentials
 export async function signInWithCredentials( prevState: unknown, formData: FormData ) {
     try {
-        console.log("Try");
         const user = signInFormSchema.parse({
             email: formData.get('email'),
             password: formData.get('password')
         });
-        console.log(user);
 
         await signIn('credentials', user);
 
         return {success: true, message: 'Signed in'}
     } catch (error) {
-        console.log('Erroe');
         if (isRedirectError(error)) throw error;
         return {success: false, message: 'Invalid credentials. Failed to sign in.'}
     }
@@ -27,4 +26,36 @@ export async function signInWithCredentials( prevState: unknown, formData: FormD
 //Sign out user
 export async function signOutUser() {
     await signOut();
+}
+
+export async function signUpUser(prevState: unknown, formData: FormData){
+    try {
+        const user = signUpFormSchema.parse({
+            name: formData.get('name'),
+            email: formData.get('email'),
+            password: formData.get('password'),
+            confirmPassword: formData.get('confirmPassword')
+        });
+
+        const plainPassword = user.password;
+
+        user.password = hashSync(user.password, 10);
+        await prisma.user.create({
+            data: {
+                name: user.name,
+                email: user.email,
+                password: user.password
+            },
+        });
+
+        await signIn('credentials', {
+            email: user.email,
+            password: plainPassword
+        });
+
+        return {success: true, message: 'User signed up successfully'}
+    } catch (error){
+        if (isRedirectError(error)) throw error;
+        return {success: false, message: 'User was not signed up'}
+    }
 }
