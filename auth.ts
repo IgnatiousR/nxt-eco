644d451d2116ae21b84717a28e652639516b5fee
Ignatious Rozario <ignatiousr80@gmail.com>
2025-07-14 12:46:51 +0600

    Update auth.ts

--- auth.ts
+++ auth.ts
@@ -5,6 +5,8 @@ import { adapter } from "next/dist/server/web/adapter";
 import CredentialsProvider from "next-auth/providers/credentials";
 import { compareSync } from "bcrypt-ts-edge";
 import type { NextAuthConfig } from "next-auth";
+import { cookies } from "next/headers";
+import { NextResponse } from "next/server";
 
 export const config = {
   pages: {
@@ -84,6 +86,15 @@ export const config = {
       }
       return token;
     },
+    authorized({request, auth}:any){
+      //Check for session cart cookie
+      if(!request.cookies.get('sessionCartId')){
+        //Generate new session cart id cookie
+        const sessionCartId = crypto.randomUUID();
+        console.log(sessionCartId);
+        return true;
+      }else return true;
+    }
   },
 } satisfies NextAuthConfig;
 
