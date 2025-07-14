28db391103fdddd674e7cbedffc1541644d65941
Ignatious Rozario <ignatiousr80@gmail.com>
2025-07-14 12:46:11 +0600

    Update page.tsx

--- app/(root)/product/[slug]/page.tsx
+++ app/(root)/product/[slug]/page.tsx
@@ -1,7 +1,7 @@
+import AddToCart from "@/components/shared/product/add-to-card";
 import ProductImages from "@/components/shared/product/product-images";
 import ProductPrice from "@/components/shared/product/product-price";
 import { Badge } from "@/components/ui/badge";
-import { Button } from "@/components/ui/button";
 import { Card, CardContent } from "@/components/ui/card";
 import { getProductBySlug } from "@/lib/actions/product.action";
 import { notFound } from "next/navigation";
@@ -64,7 +64,16 @@ const ProductDetailsPage = async (props: {
                 </div>
                 {product.stock > 0 && (
                   <div className="flex-center">
-                    <Button className="w-full">Add to Cart</Button>
+                    <AddToCart
+                      item={{
+                        productId: product.id,
+                        name: product.name,
+                        slug: product.slug,
+                        qty: 1,
+                        image: product.images![0],
+                        price: product.price,
+                      }}
+                    />
                   </div>
                 )}
               </CardContent>
