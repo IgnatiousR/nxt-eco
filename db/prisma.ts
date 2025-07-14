d7ef0bdcd50da5ea169acb2e2d402c48ea163ded
Ignatious Rozario <ignatiousr80@gmail.com>
2025-07-14 13:26:42 +0600

    Update prisma.ts

--- db/prisma.ts
+++ db/prisma.ts
@@ -1,5 +1,6 @@
 import { PrismaClient } from "@/lib/generated/prisma";
 import { neonConfig } from "@neondatabase/serverless";
+import { PrismaNeon } from '@prisma/adapter-neon';
 // import { PrismaNeon } from "@prisma/adapter-neon";
 // import { PrismaClient } from "@prisma/client";
 import ws from "ws";
@@ -13,15 +14,11 @@ neonConfig.webSocketConstructor = ws;
 
 // Instantiates the Prisma adapter using the Neon connection pool to handle the connection between Prisma and Neon.
 // const adapter = new PrismaNeon(pool);
+const connectionString = `${process.env.DATABASE_URL}`
 
+const adapter = new PrismaNeon({ connectionString })
 // Extends the PrismaClient with a custom result transformer to convert the price and rating fields to strings.
-export const prisma = new PrismaClient({
-  datasources: {
-    db: {
-      url: process.env.DATABASE_URL,
-    },
-  },
-}).$extends({
+export const prisma = new PrismaClient({ adapter }).$extends({
   result: {
     product: {
       price: {
