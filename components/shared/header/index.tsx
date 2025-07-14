e06333fc64f3bf6a6b90c35b7e1f02763d4ab6e4
Ignatious Rozario <ignatiousr80@gmail.com>
2025-07-14 10:46:07 +0600

    Moved the assets to root directory and made the header(navbar) sticky

--- components/shared/header/index.tsx
+++ components/shared/header/index.tsx
@@ -4,17 +4,27 @@ import { APP_NAME } from "@/lib/constants";
 import Menu from "./menu";
 
 const Header = () => {
-  return <header className="w-full border-b">
-    <div className="wrapper flex-between">
-      <div className="flex-start">
-        <Link href="/" className="flex-start">
-          <Image src='/images/logo.svg' alt={`${APP_NAME} logo`} height={48} width={48} priority={true}/>
-          <span className="hidden lg:block font-bold text-2xl ml-3">{APP_NAME}</span>
-        </Link>
+  return (
+    <header className="w-full border-b z-20 top-0 sticky bg-white">
+      <div className="wrapper flex-between">
+        <div className="flex-start">
+          <Link href="/" className="flex-start">
+            <Image
+              src="/images/logo.svg"
+              alt={`${APP_NAME} logo`}
+              height={48}
+              width={48}
+              priority={true}
+            />
+            <span className="hidden lg:block font-bold text-2xl ml-3">
+              {APP_NAME}
+            </span>
+          </Link>
+        </div>
+        <Menu />
       </div>
-      <Menu/>
-    </div>
-  </header>;
-}
- 
+    </header>
+  );
+};
+
 export default Header;
